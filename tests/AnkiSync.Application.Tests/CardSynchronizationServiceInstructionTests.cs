f68330421f19622c3d080596be209b1935aafecb
AaronWong <aaronwonghan@gmail.com>
2025-12-03 03:32:20 -0800

    Added new unit test cases for card movements from different decks

--- tests/AnkiSync.Application.Tests/CardSynchronizationServiceInstructionTests.cs
+++ tests/AnkiSync.Application.Tests/CardSynchronizationServiceInstructionTests.cs
@@ -204,4 +204,207 @@ public class CardSynchronizationServiceInstructionTests
         result.Should().Contain(instruction => instruction is DeleteDeckInstruction);
         result.Should().Contain(instruction => instruction is SyncWithAnkiInstruction);
     }
+
+    [Fact]
+    public async Task AccumulateInstructionsAsync_WithCardInDifferentDeck_MovesCardToCorrectDeck()
+    {
+        // Arrange - Card exists in "OldDeck" but source has it in "NewDeck"
+        var sourceCard = new QuestionAnswerCard 
+        { 
+            Question = "Test Question?", 
+            Answer = "Test Answer",
+            DateModified = DateTimeOffset.Now 
+        };
+        
+        var sourceDecks = new List<Deck>
+        {
+            new Deck
+            {
+                DeckId = DeckId.FromPath("NewDeck"),
+                Cards = new List<Card> { sourceCard }
+            }
+        };
+        
+        var existingCard = new QuestionAnswerCard 
+        { 
+            Id = 123,
+            Question = "Test Question?", 
+            Answer = "Test Answer",
+            DateModified = DateTimeOffset.Now // Same content and date, so no update needed
+        };
+        
+        var existingDecks = new List<Deck>
+        {
+            new Deck
+            {
+                DeckId = DeckId.FromPath("OldDeck"),
+                Cards = new List<Card> { existingCard }
+            },
+            new Deck
+            {
+                DeckId = DeckId.FromPath("NewDeck"),
+                Cards = new List<Card>()
+            }
+        };
+
+        // Act
+        var result = await _service.AccumulateInstructionsAsync(sourceDecks, existingDecks);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(3); // DeleteDeck (OldDeck), MoveCard, SyncWithAnki (no UpdateCard since content is same)
+        
+        var deleteInstructions = result.OfType<DeleteDeckInstruction>().ToList();
+        deleteInstructions.Should().HaveCount(1);
+        deleteInstructions[0].DeckId.Name.Should().Be("OldDeck");
+        deleteInstructions[0].DeckId.Parents.Should().BeEmpty();
+        
+        var moveInstructions = result.OfType<MoveCardInstruction>().ToList();
+        moveInstructions.Should().HaveCount(1);
+        moveInstructions[0].CardId.Should().Be(123);
+        moveInstructions[0].TargetDeckId.Name.Should().Be("NewDeck");
+        moveInstructions[0].TargetDeckId.Parents.Should().BeEmpty();
+        
+        result.Should().Contain(instruction => instruction is SyncWithAnkiInstruction);
+    }
+
+    [Fact]
+    public async Task AccumulateInstructionsAsync_WithCardInDifferentDeckAndContentChanged_UpdatesAndMovesCard()
+    {
+        // Arrange - Card exists in "OldDeck" but source has it in "NewDeck" with different content
+        var sourceCard = new QuestionAnswerCard 
+        { 
+            Question = "Test Question?", 
+            Answer = "Updated Answer", // Changed content
+            DateModified = DateTimeOffset.Now 
+        };
+        
+        var sourceDecks = new List<Deck>
+        {
+            new Deck
+            {
+                DeckId = DeckId.FromPath("NewDeck"),
+                Cards = new List<Card> { sourceCard }
+            }
+        };
+        
+        var existingCard = new QuestionAnswerCard 
+        { 
+            Id = 123,
+            Question = "Test Question?", 
+            Answer = "Old Answer", // Different content
+            DateModified = DateTimeOffset.Now.AddMinutes(-1) 
+        };
+        
+        var existingDecks = new List<Deck>
+        {
+            new Deck
+            {
+                DeckId = DeckId.FromPath("OldDeck"),
+                Cards = new List<Card> { existingCard }
+            },
+            new Deck
+            {
+                DeckId = DeckId.FromPath("NewDeck"),
+                Cards = new List<Card>()
+            }
+        };
+
+        // Act
+        var result = await _service.AccumulateInstructionsAsync(sourceDecks, existingDecks);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(4); // DeleteDeck (OldDeck), UpdateCard, MoveCard, SyncWithAnki
+        
+        var deleteInstructions = result.OfType<DeleteDeckInstruction>().ToList();
+        deleteInstructions.Should().HaveCount(1);
+        deleteInstructions[0].DeckId.Name.Should().Be("OldDeck");
+        deleteInstructions[0].DeckId.Parents.Should().BeEmpty();
+        
+        var updateInstructions = result.OfType<UpdateCardInstruction>().ToList();
+        updateInstructions.Should().HaveCount(1);
+        updateInstructions[0].CardId.Should().Be(123);
+        updateInstructions[0].Card.Should().BeOfType<QuestionAnswerCard>();
+        ((QuestionAnswerCard)updateInstructions[0].Card).Answer.Should().Be("Updated Answer");
+        
+        var moveInstructions = result.OfType<MoveCardInstruction>().ToList();
+        moveInstructions.Should().HaveCount(1);
+        moveInstructions[0].CardId.Should().Be(123);
+        moveInstructions[0].TargetDeckId.Name.Should().Be("NewDeck");
+        moveInstructions[0].TargetDeckId.Parents.Should().BeEmpty();
+        
+        result.Should().Contain(instruction => instruction is SyncWithAnkiInstruction);
+    }
+
+    [Fact]
+    public async Task AccumulateInstructionsAsync_WithCardInNestedChildDeck_MovesCardToParentDeck()
+    {
+        // Arrange - Card exists in nested "Parent::Child::Grandchild" but source has it in "Parent"
+        var sourceCard = new QuestionAnswerCard 
+        { 
+            Question = "Test Question?", 
+            Answer = "Test Answer",
+            DateModified = DateTimeOffset.Now 
+        };
+        
+        var sourceDecks = new List<Deck>
+        {
+            new Deck
+            {
+                DeckId = DeckId.FromPath("Parent"),
+                Cards = new List<Card> { sourceCard }
+            }
+        };
+        
+        var existingCard = new QuestionAnswerCard 
+        { 
+            Id = 456,
+            Question = "Test Question?", 
+            Answer = "Test Answer",
+            DateModified = DateTimeOffset.Now // Same content, so no update needed
+        };
+        
+        var existingDecks = new List<Deck>
+        {
+            new Deck
+            {
+                DeckId = DeckId.FromPath("Parent"),
+                Cards = new List<Card>()
+            },
+            new Deck
+            {
+                DeckId = DeckId.FromPath("Parent", "Child"),
+                Cards = new List<Card>()
+            },
+            new Deck
+            {
+                DeckId = DeckId.FromPath("Parent", "Child", "Grandchild"),
+                Cards = new List<Card> { existingCard }
+            }
+        };
+
+        // Act
+        var result = await _service.AccumulateInstructionsAsync(sourceDecks, existingDecks);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(3); // DeleteDeck (Grandchild only, since Child has Grandchild as child), MoveCard, SyncWithAnki
+        
+        var deleteInstructions = result.OfType<DeleteDeckInstruction>().ToList();
+        deleteInstructions.Should().HaveCount(1);
+        
+        // Check that only Grandchild deck is marked for deletion (Child is protected because it has Grandchild as child)
+        var grandchildDelete = deleteInstructions.FirstOrDefault(d => d.DeckId.Name == "Grandchild");
+        grandchildDelete.Should().NotBeNull();
+        grandchildDelete?.DeckId.Parents.Should().BeEquivalentTo(new[] { "Parent", "Child" });
+        
+        var moveInstructions = result.OfType<MoveCardInstruction>().ToList();
+        moveInstructions.Should().HaveCount(1);
+        moveInstructions[0].CardId.Should().Be(456);
+        moveInstructions[0].TargetDeckId.Name.Should().Be("Parent");
+        moveInstructions[0].TargetDeckId.Parents.Should().BeEmpty();
+        
+        result.Should().Contain(instruction => instruction is SyncWithAnkiInstruction);
+    }
 }
